fbd9b7e006f428341940dda0a0538c544eb31a25
Leandro Heck <leoheck@gmail.com>
2017-05-02 01:12:25 -0300

    Adding some install instructions

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,15 @@
 export CALIBRE_DEVELOP_FROM=$(pwd)/src
 
+# Instalation stuff
+export SW=/tmp/sw
+export PATH=$SW/bin:$PATH
+export CFLAGS=-I$SW/include
+export LDFLAGS=-L$SW/lib
+export LD_LIBRARY_PATH=$SW/qt/lib:$SW/lib
+export PKG_CONFIG_PATH=$SW/lib/pkgconfig
+export QMAKE=$SW/qt/bin/qmake
+export QT_PLUGIN_PATH=$SW/qt/plugins
+# curl https://download.calibre-ebook.com/travis/sw-linux.tar.xz | tar xJ -C /tmp
+# python setup.py bootstrap                                                                                                                               lheck@moon
+
+
